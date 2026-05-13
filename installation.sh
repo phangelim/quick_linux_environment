2d8bb46faac4ac7d35e1cc13a00e2cb52f962d5c
Pedro Henrique <phlclima2007@gmail.com>
2026-05-13 16:18:16 -0300

    installation.sh it was done

--- installation.sh
+++ installation.sh
@@ -2,4 +2,28 @@
 
 set -e
 
-# Install the packages of desktop/
+# Install the packages of terminal/
+for script in terminal/*.sh; do
+  if [ -x "$script" ]; then
+    echo "Installing ${script}"
+    "$script"
+  else
+    echo "Error!!!"
+    echo "The file $script does not have execution permissions"
+    exit 1
+  fi
+done
+
+echo "=>Terminal softwares Installed!!!"
+
+# install the packages of desktop/
+for packages in desktop/*.sh; do
+  if [ -x "$packages" ]; then
+    echo "Installing ${packages}"
+    "$packages"
+  else
+    echo "Error!!!"
+    echo "The file $packages does not have execution permissions"
+    exit 1
+  fi
+done
