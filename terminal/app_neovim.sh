ac53dbed998bd1d88336b2e4131ba3cbb2801f34
Pedro Henrique <phlclima2007@gmail.com>
2026-05-16 15:40:50 -0300

    app_neovim.sh ready

--- terminal/app_neovim.sh
+++ terminal/app_neovim.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+set -e
+
+#install neovim
 yay -S --needed --noconfirm neovim
+
+if [ -d "$HOME/.config/nvim" ]; then
+  echo "=>Creating a backup for neovim"
+  mv ~/.config/nvim ~/.config/nvim.backup
+fi
 
 # lazyvim install
 git clone https://github.com/LazyVim/starter ~/.config/nvim
