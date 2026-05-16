b77a10789a936aa2a03cf96279ada4765f109139
Pedro Henrique <phlclima2007@gmail.com>
2026-05-16 15:36:54 -0300

    zsh ready

--- terminal/app_zsh.sh
+++ terminal/app_zsh.sh
@@ -1,7 +1,14 @@
-yay -S --needed --noconfirm zsh
+#!/bin/bash
+
+set -e
+
+#install zsh and the powerlevel10k
+yay -S --needed --noconfirm zsh ttf-meslo-nerd-font-powerlevel10k
 
 #copy my config
-cp config/zshrc/.zshrc ~/.zshrc
+echo "=>Copy zsh dotfiles"
+cp config/zsh/.zshrc ~/.zshrc
 
 # set zsh as default
+echo "=>Setting the zsh as the default shell"
 sudo chsh -s $(which zsh) $USER
